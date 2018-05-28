a938b50ec06049aff8f67bae00baf1e0fa3c317c
smogathapalli <astesthyd.2@gmail.com>
2018-05-28 14:57:53 +0530

    Commit 2

--- app/src/main/java/com/example/smogathapalli/myapplication/MainActivity.java
+++ app/src/main/java/com/example/smogathapalli/myapplication/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        int x = 10;
     }
 
     @Override
